91f0ef4fc22aa930b7ce55d4adb6d8eb4a891558
JamilBadenjki <JamilBadenjki@DESKTOP-N48GC9N>
2026-06-17 02:59:01 +0300

    Fix Bugs

---
+++ Back-end/Chatting/Chatting/Services/ConnectionManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Concurrent;
+
+namespace Chatting.Services
+{
+    public class ConnectionManager
+    {
+        // UserId -> ConnectionId
+        private static readonly ConcurrentDictionary<int, string> _connections = new();
+
+        public void AddConnection(int userId, string connectionId)
+        {
+            _connections[userId] = connectionId;
+        }
+
+        public void RemoveConnection(int userId)
+        {
+            _connections.TryRemove(userId, out _);
+        }
+
+        public string? GetConnection(int userId)
+        {
+            _connections.TryGetValue(userId, out var connectionId);
+            return connectionId;
+        }
+    }
+}
